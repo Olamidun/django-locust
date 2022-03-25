4c33043613dfa880940156af72836287271f831e
Olamidun <kolapoolamidun@gmail.com>
2022-03-25 13:49:54 +0100

    created models for posts and comments, also used faker library and bulk_create function to populate the db with posts and comments

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=500)
+    body = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    body = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f"{self.user}'s comment {self.pk}"
+
+
+
---
+++ blog/post_generator.py
@@ -0,0 +1,29 @@
+from faker import Faker
+from faker.providers import DynamicProvider
+from django.contrib.auth.models import User
+
+from .models import Post, Comment
+
+fake = Faker()
+
+user = User.objects.get(pk=1)
+
+def generate_post():
+    body = [Post(title=f"title {_ + 101}", body=fake.text(), user=user) for _ in range(1000)]
+
+    Post.objects.bulk_create(body)
+    print("Done")
+
+
+def generate_comment():
+
+    body = [Comment(post=Post.objects.get(pk= _ + 400), body=fake.text(), user=user) for _ in range(500)]
+
+    Comment.objects.bulk_create(body)
+    print("Done")
+
+
+
+# now you can use:
+
+# 'dr.'
